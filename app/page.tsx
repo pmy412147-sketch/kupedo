b685a55b6e5dae11d4bbd71f32ccda0bd9ac5747
PupFi <pmy412147@gmail.com>
2025-11-12 13:53:46 +0100

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from '@/components/Header';
 import { CategoryGrid } from '@/components/CategoryGrid';
 import { FilterBar, FilterValues } from '@/components/FilterBar';
